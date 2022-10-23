84de4c3434751f68b466d864b5bd1cd5a761ec74
femas1 <federico.massara2@gmail.com>
2022-10-23 10:42:59 +0200

    resolved input problem

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const cardHolderOutput = document.querySelector('.container-gradient__front__cardBottomInfo__card-holder');
 const cardHolderInput = document.querySelector('#cardHolder');
 
+const cardNumberOutput = document.querySelector('.container-gradient__front__card-number');
+const cardNumberInput = document.querySelector('#cardNumber');
 
-cardHolderInput.addEventListener('keydown', ()=> {
+
+cardHolderInput.addEventListener('keyup', ()=> {
     let inputValue = cardHolderInput.value;
     cardHolderOutput.innerHTML = inputValue;
+})
+
+cardNumberInput.addEventListener('keyup', ()=> {
+    let inputValue = [cardNumberInput.value];
+    console.log(inputValue);
 })
