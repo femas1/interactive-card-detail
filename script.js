f6647d620a239d595dcaa22a89053f3b9ce1f9d9
femas1 <federico.massara2@gmail.com>
2022-10-23 14:19:49 +0200

    update

--- script.js
+++ script.js
@@ -4,13 +4,28 @@ const cardHolderInput = document.querySelector('#cardHolder');
 const cardNumberOutput = document.querySelector('.container-gradient__front__card-number');
 const cardNumberInput = document.querySelector('#cardNumber');
 
+let numberInput1 = document.querySelector('.numberInput--1');
+let numberInput2 = document.querySelector('.numberInput--2');
+let numberInput3 = document.querySelector('.numberInput--3');
+let numberInput4 = document.querySelector('.numberInput--4');
 
-cardHolderInput.addEventListener('keyup', ()=> {
-    let inputValue = cardHolderInput.value;
-    cardHolderOutput.innerHTML = inputValue;
-})
+
+
+// cardHolderInput.addEventListener('keyup', ()=> {
+//     let inputValue = cardHolderInput.value;
+//     cardHolderOutput.innerHTML = inputValue;
+// })
 
 cardNumberInput.addEventListener('keyup', ()=> {
-    let inputValue = [cardNumberInput.value];
-    console.log(inputValue);
+    const inputValue = [cardNumberInput.value];
+    let numberArr = parseInt(inputValue);
+    console.log(numberArr.slice(0,4))
+
+    // const input1 = numberArr.slice(0, 4);
+
+
+    // numberInput1.innerHTML = input1;
+
+    
+    
 })
